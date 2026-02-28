c9c585ab2e68a8dff769032e52f26180d1de2794
Vivek <2105vivek@gmail.com>
2026-02-28 10:43:55 +0530

    add .nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/emoji-store-demo/",
+  base: "./",
   plugins: [react()],
   server: {
     fs: {
